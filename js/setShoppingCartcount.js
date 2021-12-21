8c53b482b6ea39c79fe7ec62631fa6890c59c86e
lukaspedersen <ldp@sweetgeeks.dk>
2021-12-21 16:18:49 +0100

    .

--- js/setShoppingCartcount.js
+++
@@ -1,9 +0,0 @@
-import { ShoppingCartController } from "../controllers/shoppingCartController.js";
-import { ShoppingCartModel } from "../models/shoppingCartModel.js";
-import { ShoppingCartView } from "../views/shoppingCartView.js";
-
-const shoppingCartCountRoot = document.getElementById("shoppingCartCount");
-const shoppingCartRoot = document.getElementById("shoppingCart");
-const shoppingCartModel = new ShoppingCartModel();
-const shoppingCartView = new ShoppingCartView(null, shoppingCartCountRoot, shoppingCartRoot);
-const shoppingCartController = new ShoppingCartController(shoppingCartModel, shoppingCartView);
