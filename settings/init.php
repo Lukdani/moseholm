250179adeb885d11a504f07e162220ee50a1c623
lukaspedersen <ldp@sweetgeeks.dk>
2022-01-02 14:13:55 +0100

    db settings

--- settings/init.php
+++ settings/init.php
@@ -4,15 +4,9 @@ require $_SERVER['DOCUMENT_ROOT'] ."/moseholm/classes/classDB.php";
 define("CONFIG_LIVE", "0"); // 0: Test enviroment || 1: Live enviroment
 
 if(CONFIG_LIVE == 0){
-    $DB_SERVER = "localhost";
-    $DB_NAME = "moseholm";
-    $DB_USER = "root";
-    $DB_PASS = "";
+   
 }else{
-    $DB_SERVER = "lukdani.com.mysql:3306";
-    $DB_NAME = "lukdani_commoseholm";
-    $DB_USER = "lukdani_commoseholm";
-    $DB_PASS = "moseholm";
+
 }
 
 $db = new db($DB_SERVER, $DB_NAME, $DB_USER, $DB_PASS);
